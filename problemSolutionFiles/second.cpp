2da4623b5f40c433e57a44934c7e0ffe6a744834
Your Name <you@example.com>
2023-06-06 16:52:30 +0600

    Finally Problem 2 sovled

--- problemSolutionFiles/second.cpp
+++ problemSolutionFiles/second.cpp
@@ -1,18 +1,27 @@
-#include<iostream>
+#include <iostream>
+#include <cstring>
 using namespace std;
 
-int main() {
-    int i;
-    int counter = 0;
-    
-    for (i = 1000; i >= 1; i--) {
-        cout << i << "\t";
-        counter++;
-        
-        if (counter % 5 == 0) {
-            cout << endl;
+int main()
+{
+    int n;
+    cout << "enter the testcase number : ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        char str[100];
+        cin.get(str, 100);
+
+        int length = strlen(str);
+
+        if (str[length - 1] % 2 == 0)
+        {
+            cout << "even" << endl;
+        }
+        else
+        {
+            cout << "odd" << endl;
         }
     }
-
-    return 0;
 }
